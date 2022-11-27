e46fa9e3f0f21478a49d92edf78c5270b59a9f92
Sachin121kum <sk31817@gmail.com>
2022-11-27 15:11:35 +0530

    create database connection and schema and model

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("App is Working");
-})
+
+const connectDB = async ()=>{
+     mongoose.connect('mongodb://localhost:27017/e-comm');
+
+    const productSchema = new mongoose.Schema({});
+    const productModel = mongoose.model('products',productSchema);
+    const data = await productModel.find();
+    console.warn(data);
+}
+
+connectDB();
 
 app.listen(5000,()=>{
     console.log("Server Started");
